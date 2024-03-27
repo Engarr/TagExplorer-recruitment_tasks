c9bc90a68ed284c1f76736148242623aa03d8766
Łukasz <lukasz.poczta12@gmail.com>
2024-03-27 20:55:59 +0100

    modify image src

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,13 @@
 import { Button } from '@/components/ui/button';
 import { Link } from 'react-router-dom';
+import image from '@/assets/Stack_Overflow-Logo.wine.svg';
 
 const HomePage = () => {
   return (
     <section className='lg:h-[calc(100vh-210px)]  h-[calc(100vh-150px)] lg:min-h-[520px] flex flex-1'>
       <div className='max-w-[998px] mx-auto  w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2'>
         <div className='w-[300px]'>
-          <img src='/src/assets/Stack_Overflow-Logo.wine.svg' />
+          <img src={image} />
         </div>
         <div className='h-[300px] bg-neutral-100 w-[2px] relative top-0 hidden lg:inline-block ' />
         <div className='flex flex-col items-center lg:items-start gap-y-2 lg:gap-y-8 lg:pl-8 text-neutral-600 '>
